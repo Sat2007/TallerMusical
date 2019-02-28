057bd2ff7d092dd49ac4874097649830c25ff7ab
aulamovil07 <aulamovil07@UBAULAM07-30.salas.edu.co>
2019-02-28 09:27:16 -0500

    psar metodos de persistencia a mundo

--- TallerMusical/src/Controlador/Controlador.java
+++ TallerMusical/src/Controlador/Controlador.java
@@ -14,12 +14,13 @@ public class Controlador {
 	public Controlador() {
 
 		//aqui van los constructores tanto de Mundo, como de Vista
-		
+		int a,b;
 		bd = new Mundo();
 		gui = new Interfaz();
-
+		a=Integer.parseInt(gui.pedirDato());
+		b=Integer.parseInt(gui.pedirDato());
 		//aqui van los métodos que se invocan de Modelo (Mundo) y de Vista y que ejecutarán el programa.
-		gui.escribirDato(bd.mescla());
+		gui.escribirDato(bd.mescla(a,b));
 		/*
 		bd.setDato(gui.pedirDato());
 		bd.procesarDato();
--- TallerMusical/src/Modelo/Mundo.java
+++ TallerMusical/src/Modelo/Mundo.java
@@ -1,5 +1,7 @@
 package Modelo;
 
+import java.util.Random;
+
 //Representa el mundo del problema: las clases y métodos que describen el problema 
 //Pueden reemplazar el nombre de la clase "Mundo" por el que necesiten.
 import Modelo.Persistencia;
@@ -33,9 +35,28 @@ public class Mundo {
 	public void procesarDato() {
 		this.dato = this.dato + " procesado";
 	}
+	public String mesclaDeArreglos(int numRenglones) {
+		Random rd= new Random();
+		String rta="";
+		for(int i=0;i<numRenglones;i++) {
+		rta+=p1.FRASE1[rd.nextInt(p1.FRASE1.length)];
+		rta+=p1.FRASE2[rd.nextInt(p1.FRASE2.length)];
+		rta+=p1.FRASE3[rd.nextInt(p1.FRASE3.length)];
+		rta+=p1.FRASE4[rd.nextInt(p1.FRASE4.length)];
+		rta+=p1.FRASE5[rd.nextInt(p1.FRASE5.length)];
+		rta+=p1.FRASE6[rd.nextInt(p1.FRASE6.length)];
+		
+		}
+		return rta;
+	}
 	
-	public String mescla() {
-		return p1.mesclaDeArreglos();
+	public String mescla(int numRenglones,int numParrafos) {
+		String rta="";
+		for(int i =0;i<numParrafos;i++) {
+			rta+=mesclaDeArreglos(numRenglones);
+			rta+="\n";
+		}
+		return rta;
 	}
 	
 	
